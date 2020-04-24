4925a46016fee1de9d27f09ee3af26c3cababe98
German Cruz <cruzgerman216@yahoo.com>
2020-04-24 02:40:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,9 +45,9 @@ class Student
     i = 0
     arr = []
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    student =  self.new_from_db(row)
       if(i != num)
-        arr.push(row[i])
+        arr.push(student)
       end
       i += 1
     end
