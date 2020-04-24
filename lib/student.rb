cc0b8609da084b37e1438abe7348679ad440249a
German Cruz <cruzgerman216@yahoo.com>
2020-04-24 01:58:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,10 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+
+    sql = <<-SQL
+    SELECT * FROM student WHERE name = ? LIMIT 1
+    SQL
   end
 
   def save
