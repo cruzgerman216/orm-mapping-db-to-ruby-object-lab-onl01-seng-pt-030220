feea4e2d95ec85adc4f2469511e2a80b55403693
German Cruz <cruzgerman216@yahoo.com>
2020-04-24 02:48:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     arr
   end
 
-  def self.first_student_in_grade_10(num)
+  def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10
     SQL
