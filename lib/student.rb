fbd25830f6708bd3b009fbb1ea3e4359f66fa712
German Cruz <cruzgerman216@yahoo.com>
2020-04-24 02:08:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,13 +22,13 @@ class Student
 
   def self.all
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 9th
+    SELECT * FROM students WHERE grade = '9th'
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
-
+  
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
