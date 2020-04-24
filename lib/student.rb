fc1c41d993d0881cf0f194d07e70349a7b441c6f
German Cruz <cruzgerman216@yahoo.com>
2020-04-24 01:35:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    student = Student.new(row[0])
+    student = Student.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
   end
 
   def self.all
